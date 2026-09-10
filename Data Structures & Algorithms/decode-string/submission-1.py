c051fadbbb2c5a4feb3b81d394158b0fb4e456ac
JayceFR <110346558+JayceFR@users.noreply.github.com>
2026-09-10 13:37:35 +0100

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if c == "]":
+                # We need to pop form our stack.
+                repeatStr = ""
+                while stack and stack[-1] != "[":
+                    repeatStr = stack.pop() + repeatStr
+                stack.pop() # Removes the "]"
+                print("repeatstr", repeatStr)
+                # Get the number 
+                number = ""
+                while stack and stack[-1].isdigit():
+                    number = stack.pop() + number
+                num = int(number)
+                string = repeatStr * num 
+                for c in string:
+                    stack.append(c)
+            else:
+                stack.append(c)
+        retString = ""
+        for c in stack:
+            retString += c 
+        return retString
