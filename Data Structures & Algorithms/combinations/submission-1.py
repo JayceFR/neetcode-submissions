e7b3e3ee136bf977053b690f71c25b897e597e9a
JayceFR <110346558+JayceFR@users.noreply.github.com>
2026-09-11 13:04:10 +0100

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def backtrack(curr: int, h:int, comb: List[int]):
+            if curr > n:
+                return 
+            if h > k:
+                return 
+            if h == k:
+                # found an answer 
+                res.append(comb + [curr])
+        
+            comb.append(curr)
+            backtrack(curr+1, h+1, comb)
+            comb.pop()
+
+            backtrack(curr+1, h, comb)
+
+        backtrack(1, 1, [])
+        return res 
+
